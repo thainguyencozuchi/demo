5d2487e461657f59b168c8be7ba6580da661fc1e
NguyenThai73 <101828062+NguyenThai73@users.noreply.github.com>
2023-08-08 01:20:07 +0700

    update

---
+++ lib/modules/profile/bloc/profile_state.dart
@@ -0,0 +1,30 @@
+part of 'profile_bloc.dart';
+
+abstract class ProfileState extends Equatable {
+  const ProfileState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ProfileInitial extends ProfileState {}
+
+class ProfileLoading extends ProfileState {}
+
+class ProfileSuccess extends ProfileState {
+  final String displayName;
+
+  const ProfileSuccess({required this.displayName});
+
+  @override
+  List<Object> get props => [displayName];
+}
+
+class ProfileFailure extends ProfileState {
+  final String error;
+
+  const ProfileFailure({required this.error});
+
+  @override
+  List<Object> get props => [error];
+}
